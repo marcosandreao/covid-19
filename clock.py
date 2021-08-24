7a5fc8206660b1dcdb57aa54d5e7aa8211d4b192
Marcos Oliveira <marcosandreao@gmail.com>
2021-08-23 22:07:40 -0400

    schedule cron 12 in 12 h

--- clock.py
+++ clock.py
@@ -4,12 +4,7 @@ import brazil_total
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=30)
-def timed_job():
-    print('CRON: service is ON.')
-
-
-@sched.scheduled_job('cron', hour=3)
+@sched.scheduled_job('interval', hour=12)
 def scheduled_job():
     print('CRON: brazil_total start')
     brazil_total.run()
